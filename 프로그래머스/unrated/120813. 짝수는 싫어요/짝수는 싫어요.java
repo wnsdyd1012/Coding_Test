315dd7b3a6d1b66ef57a68c2a1b908d8ce7aeb9e
정준용 <134061759+wnsdyd1012@users.noreply.github.com>
2023-11-19 16:07:50 +0900

    [level unrated] Title: 짝수는 싫어요, Time: 0.02 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int n) {
+        int size = (n%2==0) ? n/2 : n/2+1;
+        
+        int[] answer = new int[size];
+        
+        for(int i=0; i<size; i++){
+            answer[i] = 2*i+1;
+        }
+        return answer;
+    }
+}
