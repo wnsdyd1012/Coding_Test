05e620107a6cd3c711f0e4ddc27f1e73c683f44b
정준용 <134061759+wnsdyd1012@users.noreply.github.com>
2023-12-03 15:54:28 +0900

    [level unrated] Title: 문자열 뒤집기, Time: 0.04 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181905. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(String my_string, int s, int e) {
+
+        StringBuilder sb = new StringBuilder(my_string);
+
+        String reverse =
+                new StringBuilder(my_string.substring(s,e+1)).reverse().toString();
+
+        return sb.replace(s,e+1,reverse).toString();
+    }
+}
